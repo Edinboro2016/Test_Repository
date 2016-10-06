8460a5851d71d4d3e615b61653529bd24b56f0dd
Edinboro2016 <av133768@scots.edinboro.edu>
2016-10-05 21:33:10 -0700

    Lesson2

---
+++ Function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int basicArithmetic(int number) {
+	int celcius = (number - 32) * (5/9);
+	return celcius;
+}
+
+int main() {
+	int temp= 0;
+	cout << "Hello class!" << endl;
+	count << "Please enter the temperature in Fahrenheit: ";
+	cin >> temp
+	cout << endl << "Thanks, here is what it would be in celcius!" << endl;
+	cout << basicArithmetic(number) << endl;
+	return 0;
+}
